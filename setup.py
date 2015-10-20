6163b0953492698b5088c6e5259de3f4d2822ee1
taschini <taschini@users.noreply.github.com>
2015-10-20 09:41:37 +0200

    Have Appeveyor run tests and build binary distributions for MS Windows.

--- setup.py
+++ setup.py
@@ -28,6 +28,35 @@ except ImportError:
     from distutils.core import setup, Extension
 from distutils.command.build_py import build_py
 from distutils.command.build_ext import build_ext
+from distutils import cygwinccompiler
+from distutils.ccompiler import compiler_class
+
+class Msys2CCompiler (cygwinccompiler.CygwinCCompiler):
+    # From https://github.com/aleaxit/gmpy/blob/ff2a8cca8e6f6901aa8ebb7e56a5fb19b236aaf0/msys2_build.txt
+
+    compiler_type = 'msys2'
+
+    def __init__ (self,
+                  verbose=0,
+                  dry_run=0,
+                  force=0):
+
+        cygwinccompiler.CygwinCCompiler.__init__ (self, verbose, dry_run, force)
+        shared_option = "-shared" if self.ld_version >= "2.13" else "-mdll -static"
+        entry_point = '--entry _DllMain@12' if self.gcc_version <= "2.91.57" else ''
+        common_flags = ' -O2 -Wall -fno-strict-aliasing -fwrapv'
+        import sys
+        if sys.maxsize > 2**32:
+            common_flags += ' -DMS_WIN64'
+        self.set_executables(compiler     = 'gcc'       + common_flags,
+                             compiler_so  = 'gcc -mdll' + common_flags,
+                             compiler_cxx = 'g++'       + common_flags,
+                             linker_exe='gcc',
+                             linker_so='%s %s %s' % (self.linker_dll, shared_option, entry_point))
+        self.dll_libraries=[]
+
+cygwinccompiler.Msys2CCompiler = Msys2CCompiler
+compiler_class['msys2'] = ('cygwinccompiler', 'Msys2CCompiler', "MSYS2/MinGW-w64 port of GNU C Compiler for MS Windows")
 
 class custom_build_py(build_py):
     def run(self):
